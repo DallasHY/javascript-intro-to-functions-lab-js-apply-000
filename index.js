c62654a6391b4f2495903e1f54fa611141d72894
hyde.dallas1@gmail.com <hyde.dallas1@gmail.com>
2018-07-23 05:01:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function logShout(string) {
-  console.log(string.toUpperCase())
+function sayHiToGrandma(string) {
+  console.log(string.toMixedCase())
 }
