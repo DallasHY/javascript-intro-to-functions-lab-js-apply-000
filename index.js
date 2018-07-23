e8a3cd2891d0caf5bb13f82fe1073a56bfb7be8a
hyde.dallas1@gmail.com <hyde.dallas1@gmail.com>
2018-07-23 05:12:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.UpperCase() === string ) {
+  if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }
   else if (string === "I love you, Grandma.") {
-    return "I love you too."
+    return "I love you, too."
   }
-  else {
-    return "I can't hear you!"
-  }
+   else {
+    return ("I can't hear you!")
+}
 }
